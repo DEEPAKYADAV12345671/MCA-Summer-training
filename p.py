ace47fa10b82e69f777244d069a3c54bc58fe1cf
Deepak Yadav <dy627978@gmail.com>
2025-08-12 10:37:47 +0530

    new

---
+++ p.py
@@ -0,0 +1,76 @@
+print("welcome")
+name="Deepak"
+type(name)
+age=23
+print(type(age))
+weight=4.5
+print(type)
+#list datatype
+#[val1,val2,val3]
+student=["deepak",42,45.66]
+print(type(student))
+#mapping data types
+#dictionary
+student1={"name":"pak",
+    "age":56
+}
+print(student1(age))
+#sequence data type
+#list ,tuple,range
+#tuple
+tuple=(
+    
+)
+#pop()-->remove random item
+#clear-->remove all items
+#copy()-->copy the set
+#list
+'''ordered
+changeble(mutable)
+collection item
+allow duplicates
+indexing and slicing'''
+#create list
+fruit=["apple","banana"]
+#using list function
+fruit2=list(("apple","banana"))
+
+print(fruit[0:3])
+#change elements in list
+fruit[1]="kiwi"
+print(fruit)
+#adding element
+fruit2.append("grapes")
+#specific position 
+fruit2.insert(1,"orange")
+#add multiple element
+fruit2.extend("pineapple","watermelon")
+print(fruit2)
+#remove elments 
+#by value
+fruit2.remove("mango")
+#by index
+fruit2.pop(2)
+#last element
+fruit2.pop
+#delete
+del 
+print(fruit2)
+#useful method in list
+num=[1,4,5,8,5,8,0,1]
+num.sort()
+num.sort(reverse=True)
+num.reverse()
+print()
+print(num)
+#github >is a online plateform where u can store .share and collab on project social media for the developers
+#git> version control(tool)
+#svb>subversion
+#git init
+#git remote add origin >git add>git status>git commit-m"first commit"
+
+
+
+
+
+

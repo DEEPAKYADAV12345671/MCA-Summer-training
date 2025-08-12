ff79575c89d41ef10f2b92724ea6edb1faa8e1f9
Deepak Yadav <dy627978@gmail.com>
2025-08-12 10:55:41 +0530

    sum

--- p.py
+++ p.py
@@ -1,75 +1,68 @@
 print("welcome")
-name="Deepak"
+name = "Deepak"
 type(name)
-age=23
+age = 23
 print(type(age))
-weight=4.5
-print(type)
-#list datatype
-#[val1,val2,val3]
-student=["deepak",42,45.66]
+weight = 4.5
+print(type(weight))  # Fixed: should print the type of weight, not the type function itself
+
+# list datatype
+# [val1, val2, val3]
+student = ["deepak", 42, 45.66]
 print(type(student))
-#mapping data types
-#dictionary
-student1={"name":"pak",
-    "age":56
-}
-print(student1(age))
-#sequence data type
-#list ,tuple,range
-#tuple
-tuple=(
-    
-)
-#pop()-->remove random item
-#clear-->remove all items
-#copy()-->copy the set
-#list
+
+# mapping data types
+# dictionary
+student1 = {"name": "pak", "age": 56}
+print(student1["age"])  # Fixed: use [] to access dictionary values
+
+# sequence data type
+# list, tuple, range
+# tuple
+tuple1 = ()  # Renamed to avoid confusion with built-in 'tuple'
+
+# pop() --> remove item by index
+# clear() --> remove all items
+# copy() --> copy the list
+
 '''ordered
-changeble(mutable)
+changeable (mutable)
 collection item
 allow duplicates
 indexing and slicing'''
-#create list
-fruit=["apple","banana"]
-#using list function
-fruit2=list(("apple","banana"))
+
+# create list
+fruit = ["apple", "banana"]
+# using list function
+fruit2 = list(("apple", "banana"))
 
 print(fruit[0:3])
-#change elements in list
-fruit[1]="kiwi"
+# change elements in list
+fruit[1] = "kiwi"
 print(fruit)
-#adding element
+# adding element
 fruit2.append("grapes")
-#specific position 
-fruit2.insert(1,"orange")
-#add multiple element
-fruit2.extend("pineapple","watermelon")
+# specific position 
+fruit2.insert(1, "orange")
+# add multiple elements
+fruit2.extend(["pineapple", "watermelon"])  # Fixed: extend expects an iterable, so use a list
 print(fruit2)
-#remove elments 
-#by value
-fruit2.remove("mango")
-#by index
+# remove elements 
+# by value (only if exists)
+if "mango" in fruit2:
+    fruit2.remove("mango")
+# by index
 fruit2.pop(2)
-#last element
-fruit2.pop
-#delete
-print(fruit2)
-#useful method in list
-num=[1,4,5,8,5,8,0,1]
+# last element
+fruit2.pop()
+# delete the whole list (commented out for safety)
+# del fruit2
+print(fruit2)  # Now safe to print
+
+# useful methods in list
+num = [1, 4, 5, 8, 5, 8, 0, 1]
 num.sort()
 num.sort(reverse=True)
 num.reverse()
 print()
 print(num)
-#github >is a online plateform where u can store .share and collab on project social media for the developers
-#git> version control(tool)
-#svb>subversion
-#git init
-#git remote add origin >git add>git status>git commit-m"first commit"
-
-
-
-
-
-
